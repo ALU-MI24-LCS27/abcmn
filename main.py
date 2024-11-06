b8d547f0de0d420d74b5fde650e711516ba7788e
devark28 <e.shimwa1@alustudent.com>
2024-11-06 12:22:36 +0200

    Chore: separate usage message

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+# import modules
+from modules.help_messages import usage_message
 
 # abcmn cli tool entry
 """
@@ -48,35 +50,7 @@ if __name__ == "__main__":
     elif argv[1] == "version":
         print("version")
     elif argv[1] == "help":
-        print("""
-        Usage: abcmn <command> [options]
-        Commands:
-            watch <path>\t watch a file or directory for changes
-            tasks <sub-command>\t manage the tasks
-            timer <sub-command>\t manage the timer
-            stats\t show the statistics
-        
-        Tasks sub-command:
-            list\t list all tasks
-            add\t add a new task
-            remove\t remove a task
-            update\t update a task
-            complete\t mark a task as completed
-            uncomplete\t mark a task as uncompleted
-        
-        Timer sub-command:
-            <empty>\t show the timer status
-            start\t start the timer
-            stop\t stop the timer
-            reset\t reset the timer
-        
-        Options (watch):
-            --message\t the commit message template
-        
-        Variables:
-            $file\t the changed file path
-            $date\t the current date
-        """)
+        print(usage_message)
     else:
         if argv[1] is None or argv[1] == "":
             print("Unknown command: " + argv[1])
