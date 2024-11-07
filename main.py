d3482fa59cd92f4cd2b69f1bcc523451564f0e73
devark28 <e.shimwa1@alustudent.com>
2024-11-07 22:51:32 +0200

    Chore: integrate all the modules in the entry point of the program

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import sys
+import os
 # import modules
 from modules.help_messages import usage_message
+from modules.init import init_abcmn_dir
+from modules.stats import handle_stats_command
 from modules.tasks import handle_tasks_command
+from modules.timer import handle_timer_command
 from modules.version import __version__ as app_version
+from modules.push import handle_push_command
 
 # abcmn cli tool entry point
 # read the README.md file for the tool usage information
@@ -13,18 +18,29 @@ if __name__ == "__main__":
     if argc < 2:
         print(usage_message)
         sys.exit(1)
-    if argv[1] == "watch":
-        print("watch")
+
+    if argv[1] == "init":
+        init_abcmn_dir()
+        sys.exit(0)
+
+    __abcmn_dir = os.path.join(".abcmn")
+    if not os.path.exists(__abcmn_dir):
+        print("Please run 'abcmn init' to initialize the tool")
+        sys.exit(1)
+
+    if argv[1] == "push":
+        handle_push_command(argv)
     elif argv[1] == "tasks":
         handle_tasks_command(argv)
     elif argv[1] == "timer":
-        print("timer")
+        handle_timer_command(argv)
     elif argv[1] == "stats":
-        print("stats")
+        handle_stats_command()
     elif argv[1] == "version":
         print(f"Version: {app_version}")
     elif argv[1] == "help":
         print(usage_message)
     else:
-        print("file path")
+        print(usage_message)
+        sys.exit(1)
     sys.exit(0)
